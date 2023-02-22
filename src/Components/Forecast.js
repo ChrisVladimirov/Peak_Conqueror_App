1e2148eafb664c9740f09e097862d9ef0cc9db95
Chris <sevenwonders.info@gmail.com>
2023-02-22 20:47:33 +0200

    forecast

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -4,6 +4,7 @@ import {ForecastDayDTO} from "./ForecastDayDTO";
 import {NavbarTemplate} from "./NavbarTemplate";
 import {getWeatherForLocation} from "../services/forecastService";
 import {Footer} from "./Footer";
+import {Link} from "react-router-dom";
 
 export const Forecast = () => {
 
@@ -33,12 +34,32 @@ export const Forecast = () => {
         <div>
             <NavbarTemplate/>
             <h3 style={{marginTop: '130px'}}>Forecast data for our users</h3>
-            <Suspense fallback={<p>Loading...</p>}>
-                {forecast.length > 0
-                    ? forecast.map(day => <ForecastDayDTO key={day.date_} dayDTO={day}/>)
-                    : <p>Nothing yet!</p>
-                }
-            </Suspense>
+
+            <section className="d-flex justify-content-center align-self-center" style={{margin:'100px'}}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    {forecast.length > 0
+                        ? forecast.map(day => <ForecastDayDTO key={day.date_} dayDTO={day}/>)
+                        : <p>Nothing yet!</p>
+                    }
+                </Suspense>
+            </section>
+            <section>
+                <div id="buttons" style={{margin: '75px'}}
+                     className="btn-toolbar d-flex justify-content-around position-relative" role={"toolbar"}>
+                    <div className="btn-group" role={"group"}>
+                        <Link id="button-seven-days"
+                              className="btn btn-dark position-absolute top-50 start-0 translate-middle"
+                              to="/weather/{mountain}/{location}/7">View 7-day forecast</Link>
+                    </div>
+                    <div className="btn-group" role="group">
+                        <Link id="button-ten-days"
+                              className="btn btn-dark position-absolute top-50 start-100 translate-middle"
+                              to="/weather/{mountain}/{location}/10">
+                            View complete 10-day forecast
+                        </Link>
+                    </div>
+                </div>
+            </section>
             <Footer/>
         </div>
     );
--- src/Components/ForecastDayDTO.js
+++ src/Components/ForecastDayDTO.js
@@ -1,18 +1,70 @@
-export const ForecastDayDTO = (/*{
-                                   date_, tempMax, tempMin, humidityPercentage,
-                                   snowPercentage, windSpeed, cloudCover,
-                                   visibilityPercentage, precipitationProb, summary_, iconName}*/props) => {
+import {WeatherImage} from "./WeatherImage";
+import {Link} from "react-router-dom";
+
+export const ForecastDayDTO = (props) => {
+    const dayDTO = props.dayDTO;
+
     return (
-        <div>
-            <h3>{props.dayDTO}</h3>
-        </div>
+        <>
+            <div className="card-group">
+                <div className="card" style={{width: '18rem', backgroundColor: 'rgba(255, 248, 211, 0.4)'}}>
+                    <div className="card-body">
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <h5 className="card-title">{dayDTO.date_.weekday}</h5>
+                                <h6 className="card-subtitle">{dayDTO.date_}</h6>
+                                <div>
+                                    {
+                                        {
+                                            'cloudy': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/cloudy_xw1y6k.png"
+                                                alt="cloudy-weather-image"/>,
+                                            'partly-cloudy-day': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/partsun_z8zhx1.png"
+                                                alt="partly-cloudy-weather-image"/>,
+                                            'clear-day': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/sun_lq779u.png"
+                                                alt="clear-day-weather-image"/>,
+                                            'rain': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/rain_nzaxx7.png"
+                                                alt="rainy-weather-image"/>,
+                                            'snow': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/snow_y4xxz0.png"
+                                                alt="snowy-weather-image"/>,
+                                            'wind': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/wind_ve43qb.png"
+                                                alt="windy-weather-image"/>,
+                                            'fog': <WeatherImage
+                                                src="https://res.cloudinary.com/dhr071bhp/image/upload/v1672601794/peak-climber-pictures/weather-icons/fog_tfeh9b.png"
+                                                alt="foggy-weather-image"/>
+                                        }[dayDTO.iconName]
+                                    }
+                                </div>
+                                <p className="card-text">
+                                    {dayDTO.tempMin}° | {dayDTO.tempMax}°
+                                </p>
+                                <p className="card-text">{dayDTO.summary_}</p>
+                                <span className="card-text">
+                                <b>Wind speed: {dayDTO.windSpeed} m/s</b>
+                            </span>
+                            </li>
+                            <li className="list-group-item">
+                                <p className="card-text">&#x2614;{dayDTO.precipitationProb}%</p>
+                                <p className="card-text">Humidity: {dayDTO.humidityPercentage}%</p>
+                                <p className="card-text">&#x2601; {dayDTO.cloudCover}%</p>
+                                <p className="card-text">Visibility: {dayDTO.visibilityPercentage}%</p>
+                            </li>
+                            <li className="list-group-item">
+                                {dayDTO.snowPercentage > 0
+                                    ? <p className="card-text">Snow cover: {dayDTO.snowPercentage} mm</p>
+                                    : null
+                                }
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 }
-
-//{"date_":"2023-02-21","tempMax":19.9,
-// "tempMin":1.9,
-// "humidityPercentage":45.9,"snowPercentage":0.0,
-// "windSpeed":24.6,"cloudCover":47.9,"visibilityPercentage":13.5,
-// "precipitationProb":0.0,
-// "summary_":"Partly cloudy throughout the day.",
-// "iconName":"partly-cloudy-day"}
