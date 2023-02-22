36d3b8afc60dff78a672e632a9c7c26abe24c416
Chris <sevenwonders.info@gmail.com>
2023-02-22 19:41:51 +0200

    small refactorings

--- src/services/forecastService.js
+++ src/services/forecastService.js
@@ -1,6 +1,6 @@
 const baseUrl = "http://localhost:8081/weather/";
 
-export const getWeatherForLocation = async (mountain='Rila', location='Костенец', days=5) => {
+export const getWeatherForLocation = async (mountain='Rila', location='Костенец', days=3) => {
     let result;
     if (!!mountain && !!location && !!days) {
         try {
