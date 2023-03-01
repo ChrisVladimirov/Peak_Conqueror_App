8c6492ba219f3f15639bf381d35bd866e6372926
Chris <sevenwonders.info@gmail.com>
2023-03-02 00:13:30 +0200

    closes #4

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -7,22 +7,6 @@ const endpoints = {
     'getAllUsers': '/users/all'
 }
 
-export const login = async (userData) => {
-    try {
-        await fetch(baseUrl + 'login', {
-            method: 'post',
-            body: JSON.stringify(userData),
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-}
-
 export const getAllUsers = async () => {
     return await get(endpoints.getAllUsers);
 }
