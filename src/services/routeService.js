428db5079f1e68305820f9557c132ec13b046e79
Chris <sevenwonders.info@gmail.com>
2023-03-02 21:37:30 +0200

    fixes #5

--- src/services/routeService.js
+++ src/services/routeService.js
@@ -1,23 +1,36 @@
 import {del, get, post, put} from '../api/request.js';
+import {getUserData} from "../api/util";
 
 const endpoints = {
     'routesAll': '/routes/all',
     'particularRoute': (routeId) => `/routes/${routeId}`,
+    'getLikesForRoute': (routeId) => `/routes/${routeId}/likes`,
+    'likeARoute': (userId, routeId) => `/routes/${routeId}/like?user=${userId}`,
     'createRoute': '/routes/create',
     'editRoute': '/routes/edit',
     'deleteRoute': (routeId) => `/routes/delete/${routeId}`
 }
 
 export async function getAllRoutes() {
-    return await get(endpoints.routesAll).json();
+    return await get(endpoints.routesAll);
 }
 
-export async function getParticularRoute(routeId) {
-    return await get(endpoints.particularRoute(routeId)).json();
+export function getParticularRoute(routeId) {
+    return get(endpoints.particularRoute(routeId));
+}
+
+export async function getLikesForRoute(routeId) {
+    return get(endpoints.getLikesForRoute(routeId));
 }
 
 export async function createARoute(routeData) {
     await post(endpoints.createRoute, routeData);
+}
+
+export async function likeARoute(routeId) {
+    let userData = getUserData();
+    const userId = userData.id;
+    await post(endpoints.likeARoute(userId, routeId));
 }
 
 export async function editRoute(newRouteData) {
