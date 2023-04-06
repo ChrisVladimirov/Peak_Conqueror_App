fed7d6c1b53573990be17ae2b2376e2d46e5a1b0
Chris <sevenwonders.info@gmail.com>
2023-04-06 23:46:03 +0300

    background image urls encapsulated

---
+++ src/api/constants.js
@@ -0,0 +1,9 @@
+export const pictureUrls = {
+    'LAKE': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1672599088/peak-climber-pictures/lake_kzbf4l.jpg',
+    'CHAKAR_VOIVODA': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1672598736/peak-climber-pictures/chakar-voivoda_fej8td.jpg',
+    'BY_THE_FIRE': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1672598737/peak-climber-pictures/by-the-fire_gpbzvq.jpg',
+    'PAZAR_DERE': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1672599096/peak-climber-pictures/PazarDere_rh0rfs.jpg',
+    'FERDINANDOVO_EZERO': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1677864431/Ferdinandovo_ezero.jpg',
+    'FIERY_CLOUDS': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1672599535/peak-climber-pictures/weather-images/firy-dark-clouds_wcmr1u.jpg',
+    'BABREKA_LAKE': 'https://res.cloudinary.com/dhr071bhp/image/upload/v1672599122/peak-climber-pictures/Babreka_rxzixu.jpg'
+}
